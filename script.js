c5811576e5c164bf2c3c74050450d8940ddfe137
mdwasim-github <mdwasim336784@gmail.com>
2026-02-21 23:56:24 +0530

    Website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Smooth scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
